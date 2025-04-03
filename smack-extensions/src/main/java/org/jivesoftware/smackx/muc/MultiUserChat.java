8e3a35f1243bf687f2b188dfce015e783f2827b2
Florian Schmaus <flo@geekplace.eu>
2025-04-03 10:07:34 +0200

    [muc] Make MultiUserChat.destroy() synchronized
    As all MUC state changing operations, destory() should also be
    synchronized.

--- smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChat.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/muc/MultiUserChat.java
@@ -941,7 +941,7 @@ public class MultiUserChat {
      * @throws NotConnectedException if the XMPP connection is not connected.
      * @throws InterruptedException if the calling thread was interrupted.
      */
-    public void destroy(String reason, EntityBareJid alternateJID) throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+    public synchronized void destroy(String reason, EntityBareJid alternateJID) throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
         MUCOwner iq = new MUCOwner();
         iq.setTo(room);
         iq.setType(IQ.Type.set);
